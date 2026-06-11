f3eb83094e9424a2db94dd4d760fdc73df6319c7
gurbaksh <gurbaksh.mvteams@gmail.com>
2026-06-11 17:06:26 +0530

    uydu,tjd

--- script.js
+++ script.js
@@ -10,19 +10,30 @@ const readingTimeDisplay = document.getElementById('readingTimeDisplay')
 const inputLimitCheckbox = document.getElementById('inputLimitCheckbox')
 const inplimitDiv = document.querySelector('.input-limit')
 const densityList = document.getElementById('densityList')
-
+const changeThemeButton = document.getElementById('changeThemeButton')
+const seeMoreBtn = document.querySelector('.see-more-btn')
+let seeMoreValue = false
+changeThemeButton.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark')
+})
+// changeThemeButton.addEventListener('click', () => {
+//     document.documentElement.classList.toggle('dark')
+// })
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (isDarkMode) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
 let inpLimitValue = 0;
 const readTotalCharactersPerMinute = 500;
 let inputLimitCheckboxValue = "";
 let excludeCheckbox = "";
-
-
 inputLimit.addEventListener('input', (e) => {
     inpLimitValue = e.target.value;
     const impv = inputbox.maxLength = inpLimitValue
     characterLimitCount.innerText = inpLimitValue
 })
-
 inputLimitCheckbox.addEventListener("input", (e) => {
     inputLimitCheckboxValue = e.target.checked;
     if (inputLimitCheckboxValue) {
@@ -31,7 +42,6 @@ inputLimitCheckbox.addEventListener("input", (e) => {
         inplimitDiv.classList.remove('active')
     }
 })
-
 function checkbox() {
     if (excludeCheckbox) {
         const excludeSpace = inputbox.value.split(' ')
@@ -44,7 +54,6 @@ function checkbox() {
         totalCharacters = inputbox.value.length
         totalCharactersDisplay.innerText = totalCharacters
     }
-
 }
 spacesCheckbox.addEventListener('input', (e) => {
     excludeCheckbox = e.target.checked;
@@ -53,7 +62,6 @@ spacesCheckbox.addEventListener('input', (e) => {
 
 inputbox.addEventListener('input', (e) => {
     const inputValue = e.target.value
-
     checkbox()
     // input limit warning 
     if (inpLimitValue > 0 && inpLimitValue <= totalCharacters && inputLimitCheckboxValue) {
@@ -61,30 +69,25 @@ inputbox.addEventListener('input', (e) => {
     } else {
         warningPara.classList.remove('active')
     }
-
     let spaces = inputValue.split(' ')
     const filteredSpaces = spaces.filter((space) => {
         return space != ""
     })
     wordCountDisplay.innerText = filteredSpaces.length
-
     const sentences = inputValue.split('.')
     const filteredSentences = sentences.filter((sentence) => {
         return sentence
     })
     sentenceCountDisplay.innerText = filteredSentences.length
 
-
     let wpm = inputValue.length / readTotalCharactersPerMinute
     // console.log(Math.ceil(wpm))
     readingTimeDisplay.innerText = Math.ceil(wpm);
-
 
     // letter density filter
     var count = 0
     const inputArrayValues = inputValue.split('');
     const letterDensity = {};
-
     inputArrayValues.forEach((value) => {
         const upperCaseValue = value.toUpperCase();
 
@@ -97,27 +100,44 @@ inputbox.addEventListener('input', (e) => {
             };
         }
     });
+    densityFnc(inputArrayValues, letterDensity);
+})
 
+const densityFnc = (inputArrayValues, letterDensity) => {
     const letterDensityAlphabets = Object.values(letterDensity);
+
     const filteredAlphabets = letterDensityAlphabets.filter((alphabet) => {
-        return alphabet != " ";
+        // return alphabet.letter != " " && alphabet.letter != ".";
+        return /^[a-zA-Z]$/.test(alphabet.letter);
+    })
+    const filteredWidth = inputArrayValues.filter((width) => {
+        return /^[a-zA-Z]$/.test(width);
     })
 
-    console.log("Alphabets", filteredAlphabets);  
-
-    densityList.innerHTML = letterDensityAlphabets.map((alphabet) => {
-        const width = (alphabet.count / inputValue.length) * 100;
-        // console.log(width.toFixed(2));
-
-        return `
+    if (filteredAlphabets.length > 0) {
+        densityList.innerHTML = filteredAlphabets.slice(0, seeMoreValue ? filteredAlphabets.length : 5).map((alphabet) => {
+            const width = (alphabet.count / filteredWidth.length) * 100;
+            // console.log(width.toFixed(2));
+            return `
          <div class="density flex-vertical-center">
             <span class="letter">${alphabet.letter}</span>
             <div class="progress-bar"><span style="width: ${width}%;"></span></div> 
-            <span class="count ">${alphabet.count} times</span>
+            <span class="count ">${alphabet.count} (${width.toFixed(2)}%)</span>
         </div>
-    `
-            ;
-    }).join('')
+    `;
+        }).join('');
+    } else {
+        densityList.innerHTML = `<p>No characters found. Start typing to see letter density.</p>`
+    }
+    if (filteredAlphabets.length > 5) {
+        seeMoreBtn.classList.add('see-more-btn-active')
+    }
+    else {
+        seeMoreBtn.classList.remove('see-more-btn-active')
+    }
 
-
+}
+seeMoreBtn.addEventListener('click', () => {
+    seeMoreValue = !seeMoreValue
+      seeMoreBtn.innerText = seeMoreValue ? "See Less" : "See More";
 })
